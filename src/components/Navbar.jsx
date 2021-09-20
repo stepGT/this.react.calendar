2aabb6d28886c1ddf272546bfa5d083a6023b699
stepGT <stepGT@mail.ru>
2021-09-21 00:57:20 +1000

    Added useHistory Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,35 @@
 import React, { FC } from 'react';
 import { Layout, Row, Menu } from 'antd';
+import { useHistory } from 'react-router-dom';
+import { RoutesNames } from '../router';
 
 const Navbar: FC = () => {
+    const router = useHistory();
+    const auth   = true;
     return (
         <Layout.Header>
             <Row justify="end">
-                <Menu theme="dark" mode="horizontal">
-                    <Menu.Item key="1">
-                        Login
-                    </Menu.Item>
-                </Menu>
+                {
+                    auth ?
+                        <>
+                            <div style={{ color: 'white' }}>stepGT</div>
+                            <Menu theme="dark" mode="horizontal" selectable={false}>
+                                <Menu.Item
+                                    onClick={() => { console.log('Logout') }}
+                                    key="1">
+                                    Logout
+                                </Menu.Item>
+                            </Menu>
+                        </>
+                        :
+                        <Menu theme="dark" mode="horizontal" selectable={false}>
+                            <Menu.Item
+                                onClick={router.push(RoutesNames.LOGIN)}
+                                key="1">
+                                Login
+                            </Menu.Item>
+                        </Menu>
+                }
             </Row>
         </Layout.Header>
     );
