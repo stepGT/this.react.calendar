dd0f3765890b6568bc05cab3f453e32650049441
Zuev Andrey <zuevas3@sovcombank.ru>
2021-12-07 12:33:27 +1000

    Added modal

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,8 +1,23 @@
-import { FC } from 'react';
+import { Button, Layout, Modal, Row } from 'antd';
+import { FC, useState } from 'react';
 import EventCalendar from '../components/EventCalendar';
 
 const Event: FC = () => {
-  return <EventCalendar events={[]} />;
+  const [modalVisible, setModalVisible] = useState(false);
+  return (
+    <Layout>
+      <EventCalendar events={[]} />
+      <Row justify="center">
+        <Button onClick={() => setModalVisible(true)}>Add event</Button>
+      </Row>
+      <Modal
+        title="Add event"
+        visible={modalVisible}
+        footer={null}
+        onCancel={() => setModalVisible(false)}
+      ></Modal>
+    </Layout>
+  );
 };
 
 export default Event;
