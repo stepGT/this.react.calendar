4538b28990660ca0ba4bb565c108ac3d919b9ee6
Zuev Andrey <zuevas3@sovcombank.ru>
2021-12-08 17:12:32 +1000

    Added createEvent method

--- src/store/reducers/event/action-creators.ts
+++ src/store/reducers/event/action-creators.ts
@@ -21,4 +21,15 @@ export const EventActionCreators = {
       console.log(e);
     }
   },
+  createEvent: (event: IEvent) => async (dispatch: AppDispatch) => {
+    try {
+      const events = localStorage.getItem('events') || '[]';
+      const json = JSON.parse(events) as IEvent[];
+      json.push(event);
+      dispatch(EventActionCreators.setEvents(json));
+      localStorage.setItem('events', JSON.stringify(json));
+    } catch (e) {
+      console.log(e);
+    }
+  },
 };
